2b55a9e6118f93be2111e7eea06a13c75c095d8f
gabrieldansereau <gabriel.dansereau@hotmail.com>
2019-10-15 13:18:56 -0400

    :sparkles: add new dplyr translations

--- dplyr-manipulation-translation_2019-10-08.jl
+++ dplyr-manipulation-translation_2019-10-08.jl
@@ -48,11 +48,133 @@ end
 mean(gapminder[gapminder.continent .== "Asia", :gdpPercap])
 
 
+#### The `dplyr` package
+begin
+    R"""
+    library(dplyr)
+    """
+end
 
+## Using `select()`
+begin
+    R"""
+    year_country_gdp <- select(gapminder, year, country, gdpPercap)
+    """
+end
+year_country_gdp = select(gapminder, [:year, :country, :gdpPercap])
 
+begin
+    R"""
+    year_country_gdp <- gapminder %>% select(year, country, gdpPercap)
+    """
+end
+year_country_gdp = gapminder |> x -> select(x, [:year, :country, :gdpPercap])
+# Pipe.jl offers an alternative syntax
+#=
+using Pipe
+@pipe year_country_gdp <- gapminder |> select(_, [:year, :country, :gdpPercap])
+=#
 
+## Using `filter()`
+begin
+    R"""
+    year_country_gdp_euro <- gapminder %>%
+        filter(continent == "Europe") %>%
+        select(year, country, gdpPercap)
+    """
+end
+# Exact translation
+year_country_gdp_euro = gapminder |>
+    x -> filter(y -> y.continent == "Europe", x) |>
+    x -> select(x, [:year, :country, :gdpPercap])
+# Julia-esque translation
+year_country_gdp_euro = filter(y -> y.continent == "Europe", gapminder)
+# OR
+year_country_gdp_euro = copy(gapminder)
+filter!(y -> y.continent == "Europe", year_country_gdp_euro)
+# Then
+select!(year_country_gdp_euro, [:year, :country, :gdpPercap])
 
-## The `dplyr` package
+## Challenge 1
+begin
+    R"""
+    year_country_lifeExp_Africa <- gapminder %>%
+                           filter(continent == "Africa") %>%
+                           select(year, country, lifeExp)
+    """
+end
+year_country_lifeExp_africa = gapminder |>
+    x -> filter(y -> y.continent == "Africa", x) |>
+    x -> select(x, [:lifeExp, :country, :year])
 
-# Assignment for next week:
-# translate select(), filter(), group_by() and summarize() exercices
+# Using `group_by` and `summarize`
+begin
+    R"""
+    str(gapminder)
+    """
+end
+describe(gapminder)
+
+begin
+    R"""
+    str(gapminder %>% group_by(continent))
+    """
+end
+groupby(gapminder, :continent)
+# no exact translation for `str()`
+
+begin
+    R"""
+    gdp_bycontinents <- gapminder %>%
+    group_by(continent) %>%
+    summarize(mean_gdpPercap = mean(gdpPercap))
+    """
+end
+# Exact translation
+gdp_bycontinents = gapminder |>
+    x -> groupby(x, :continent) |>
+    x -> [mean(y.gdpPercap) for y in x]
+# Julia-esque translaton 1
+by(gapminder, :continent, mean_gdpPercap = :gdpPercap => mean)
+
+# Challenge 2
+begin
+    R"""
+    lifeExp_bycountry <- gapminder %>%
+        group_by(country) %>%
+        summarize(mean_lifeExp = mean(lifeExp))
+    """
+    R"""
+    lifeExp_bycountry %>%
+        filter(mean_lifeExp == min(mean_lifeExp) | mean_lifeExp == max(mean_lifeExp))
+    """
+end
+lifeExp_bycountry = by(gapminder, :country, mean_lifeExp = :lifeExp => mean)
+sort(lifeExp_bycountry, :mean_lifeExp)[[1,end],:]
+#
+
+begin
+    R"""
+    gdp_bycontinents_byyear <- gapminder %>%
+        group_by(continent, year) %>%
+        summarize(mean_gdpPercap = mean(gdpPercap))
+    """
+end
+by(gapminder, [:continent, :year], mean_gdpPercap = :gdpPercap => mean)
+
+begin
+    R"""
+    gdp_pop_bycontinents_byyear <- gapminder %>%
+    group_by(continent, year) %>%
+        summarize(mean_gdpPercap = mean(gdpPercap),
+                  sd_gdpPercap = sd(gdpPercap),
+                  mean_pop = mean(pop),
+                  sd_pop = sd(pop))
+    """
+end
+gdp_pop_bycontinents_byyear = by(gapminder, [:continent, :year], [:gdpPercap, :pop] =>
+                                    x -> (mean_gdpPercap = mean(x.gdpPercap),
+                                          sd_gdpPercap = std(x.gdpPercap),
+                                          mean_pop = mean(x.pop),
+                                          sd_pop = std(x.pop)));
+sort(gdp_pop_bycontinents_byyear, :continent)
